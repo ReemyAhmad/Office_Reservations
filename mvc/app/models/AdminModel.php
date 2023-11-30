7a083c5e04b6db453062f7bf4a28c614ac71f77b
ReemyAhmad <remyahmad222@gmail.com>
2023-11-30 23:14:15 +0300

    fix model Admin

--- mvc/app/models/AdminModel.php
+++ mvc/app/models/AdminModel.php
@@ -1,40 +1,38 @@
 <?php
 class AdminModel {
     private $db;
+
     public function __construct($db) {
         $this->db = $db;
     }
-    public function getAdmins ($id = null){
-        if (isset($id)) {
-            $this->db->where("id",$id);
-            return $this->db->get('admin');
-        }
-        else {
-            return $this->db->get('admin');
-        }
-        
+    public function getAdminByid($id){
+        $this->db->where('id',$id);
+        return $this->db->getOne('admin');
     }
-    public function getAdminsByemail ($email = null){
-        if (isset($email)) {
-            $this->db->where("email",$email);
-            return $this->db->get('admin');
-        }
-        
-        
+    public function getAdminBydata($email,$password){
+        $this->db->where('email',$email);
+        $this->db->where('password',$password);
+        return $this->db->getOne('admin');
     }
-    public function addAdmin($data){
-        return $this->db->insert('admin', $data);
-
+    public function getAdmins(){
+        return $this->db->get('admin');
     }
-    public function UpdataAdmin($data,$id){
-        $this->db->where('id',$id );
+    public function getCard($card){
+        $this->db->where('card',$card);
+        return $this->db->getOne('admin');
+    }
+    public function updateAdmin($id,$data){
+        $this->db->where('id',$id);
         return $this->db->update('admin', $data);
     }
-    public function deleteAdmin($id){
-        $this->db->where('id', $id);
-        return $this->db->delete('admin');
-        
+    public function addAdmin($data){
+        return $this->db->insert('admin',$data);
     }
+    public function deleteAdmin($id){
+        $this->db->where('id',$id);
+        return $this->db->delete('admin');
+    }
+      
 }
 
 ?>
